88e7ca4a6ff957ac51bc42d25182729187772b37
Reimnop <rayngu2005@gmail.com>
2023-11-23 21:02:41 +0700

    Add pages

--- src/lib/component/NavLink.svelte
+++ src/lib/component/NavLink.svelte
@@ -4,10 +4,10 @@
     export let href: string;
     export let title: string;
 
-    const focused = $page.url.pathname === href;
+    $: focused = $page.url.pathname === href;
 </script>
 
-<a href={href} class="no-underline flex flex-col items-center [&>div]:hover:w-full {focused ? "[&>div]:w-full" : ""}">
-    <span class="font-bold text-lg">{title}</span>
+<a href={href} class="no-underline flex flex-col items-center [&>div]:hover:w-full [&>span]:hover:-translate-y-1 {focused ? "[&>div]:w-full" : ""}">
+    <span class="duration-150 font-bold text-lg">{title}</span>
     <div class="duration-150 origin-center h-1 w-0 bg-primary rounded-full"></div>
 </a>
