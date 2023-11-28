424f372241eac1ff1b31b53ea7a4f2e7458ddef1
Reimnop <rayngu2005@gmail.com>
2023-11-28 11:29:24 +0700

    Fix PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import { page } from '$app/stores';
+	import { pwaInfo } from 'virtual:pwa-info'; 
 	import Header from '$lib/component/Header.svelte';
 	import Footer from '$lib/component/Footer.svelte';
 
@@ -8,9 +9,12 @@
 	$: title = `${$page.data.title ?? 'Untitled'} | HCMUT Maps & Guides`;
 	$: description = $page.data.description ?? 'HCMUT Maps & Guides';
 	$: url = `https://hcmut.reimnop.com${pathname}`;
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 </script>
 
 <svelte:head>
+	{@html webManifestLink}
+
 	<title>{title}</title>
 	<meta property="og:title" content={title} />
 	<meta property="twitter:title" content={title} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA()
+		SvelteKitPWA({
+			srcDir: 'src'
+		})
 	]
 });
